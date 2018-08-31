3453b76343b7ad6f951bfb60f7f3a08b36206bc7
Starovoitov <arteom.starovoitov@yandex.ru>
2018-08-31 19:07:37 +0300

    logging and config file are added + parsing of config file

--- uWSGI_weather.py
+++ uWSGI_weather.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
+import logging
 import json
 import re
 
 
-def api_get_request(url, args):
+def api_get_request(url, args, log):
     """returns response of GET request to url and pass args as parameters"""
     get_params = urllib.urlencode(args)
     req = urllib2.Request(url + '?' + get_params)
+    log.debug(req.get_full_url() + ' - full request to service')
     opener = urllib2.build_opener()
     opener.addheaders = [('User-Agent', 'curl/7.37.0')]
     response = opener.open(req)
-
     return json.loads(response.read())
 
 
@@ -71,29 +72,78 @@ def build_response(api_data):
     return response
 
 
+def read_conf(opts, conf):
+    """read conf file and initialize opts settings"""
+    settings = []
+
+    try:
+        config_file = open(conf, 'r')
+        if config_file.mode == 'r':
+            settings = config_file.read().split("\n")
+        config_file.close()
+    except IOError or EOFError:
+        return False
+
+    for line in settings:
+        if not line.lstrip().startswith('#') and line is not "":
+            file_opt = line.split("=")
+            if opts[file_opt[0]]:
+                opts[file_opt[0]] = file_opt[1]
+    return True
+
+
 def application(environ, start_response):
 
+    opts = {
+        "config": "/usr/local/etc/uwsgi.conf",
+        "log_level": "INFO",
+        "log_path": "/var/log/uwsgi_weather.log",
+        "token": "6d867ad27aea75",
+        "appid": "972c2264ea91f1f814dbe62f5208c9e1",
+        "location_determination": "https://ipinfo.io",
+        "weather_determination": "https://api.openweathermap.org/data/2.5/weather"
+    }
+
+    conf_status = read_conf(opts, opts["config"])
+
+    logging.basicConfig(filename=opts["log_path"],
+                        level=logging.getLevelName(opts['log_level']),
+                        format='[%(asctime)s] %(levelname).1s %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S')
+
+    if not conf_status:
+        logging.critical("config file is not file or corrupted - default values in use")
+
     # authorization tokens, taken from credentials given by api
-    ipinfo_args = {'token': '6d867ad27aea75'}
-    openweather_args = {'appid': '972c2264ea91f1f814dbe62f5208c9e1'}
+    ipinfo_args = {'token': opts['token']}
+    openweather_args = {'appid': opts['appid']}
 
     # api urls
-    location_determination = 'https://ipinfo.io'
-    weather_determination = 'https://api.openweathermap.org/data/2.5/weather'
+    location_determination = opts['location_determination']
+    weather_determination = opts['weather_determination']
 
     try:
         ip = re.search('(?<=ip2w/)[0-9]+(?:\.[0-9]+){3}', environ['PATH_INFO'])
         if ip:
             location_determination += ('/' + ip.group(0))
+            logging.info("ip address for location of weather is found")
+        else:
+            logging.info("No ip address is given - weather determination "
+                         "will use ip address of wsgi service")
 
         start_response('200 OK', [('Content-Type', 'text/html')])
-        lat, lon = api_get_request(location_determination, ipinfo_args)['loc'].split(',')
+
+        lat, lon = api_get_request(location_determination, ipinfo_args, logging)['loc'].split(',')
+        logging.info("connection to location determination - " +
+                     location_determination + " is successful")
 
         openweather_args['lat'] = lat
         openweather_args['lon'] = lon
-
-        data = api_get_request(weather_determination, openweather_args)
+        data = api_get_request(weather_determination, openweather_args, logging)
+        logging.info("connection to weather determination - " +
+                     weather_determination + " is successful")
 
         return build_response(data)
     except urllib2.URLError:
+        logging.critical("Connection to weather api failed")
         return ["<h3 style='color:red'>Connection to weather api failed</h3>"]
